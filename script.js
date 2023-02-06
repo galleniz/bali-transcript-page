b2f7e7f0fbad4886a6b824c799cab312b40d6bb5
Nis <jalipa7u7@gmail.com>
2023-02-05 19:34:26 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,123 @@
+let data = {"guild": "eyAiZ3VpbGROYW1lIjogIk1penlpY28gQ29tbXVuaXR5IiwgImd1aWxkSWQiOiAiOTczMDIxOTQyODkyMzU5NzExIiwgImd1aWxkSWNvbiI6ICJodHRwczovL2Nkbi5kaXNjb3JkYXBwLmNvbS9pY29ucy85NzMwMjE5NDI4OTIzNTk3MTEvZGFlZDVkN2NkYWMwNTk0NTEzNTRjODIwZjNhNTU3ZWMud2VicCIsICJjaGFubmVsIjogInRlc3RpbmctZXhwb3J0LXN5c3RlbSJ9IA==", "messages": "W3siY29udGVudCI6ImFzZCIsImF1dGhvciI6Ik1yTml6IzI2MjgiLCJ0aW1lc3RhbXAiOjE2NzU2NDYyMDA1MDcsImF1dGhvckljb25VUkwiOiJodHRwczovL2Nkbi5kaXNjb3JkYXBwLmNvbS9hdmF0YXJzLzQ4MjYzNzgwNTAzNDY2ODAzMi8zMmY4MWIxYWU1ZGFhMzdiNGFiYzhkNzQxZTQ1ZTM5NS53ZWJwIiwiYXR0YWNoZW1lbnRzIjpbXSwiYm90IjpmYWxzZX0seyJjb250ZW50IjoiIiwiYXV0aG9yIjoiTXJOaXojMjYyOCIsInRpbWVzdGFtcCI6MTY3NTY0NjIxNDQ4MSwiYXV0aG9ySWNvblVSTCI6Imh0dHBzOi8vY2RuLmRpc2NvcmRhcHAuY29tL2F2YXRhcnMvNDgyNjM3ODA1MDM0NjY4MDMyLzMyZjgxYjFhZTVkYWEzN2I0YWJjOGQ3NDFlNDVlMzk1LndlYnAiLCJhdHRhY2hlbWVudHMiOlt7InR5cGUiOiJpbWFnZSIsInVybCI6Imh0dHBzOi8vY2RuLmRpc2NvcmRhcHAuY29tL2F0dGFjaG1lbnRzLzEwNzE4NjU3MTEyMDAzMjU3MTIvMTA3MTk2MjY2MDc3OTg1MTgzNy9wYTBsYnVmMWl2bDUxLmpwZyJ9XSwiYm90IjpmYWxzZX0seyJjb250ZW50IjoiLS1leHBvcnQiLCJhdXRob3IiOiJNck5peiMyNjI4IiwidGltZXN0YW1wIjoxNjc1NjQ2MjI0ODM3LCJhdXRob3JJY29uVVJMIjoiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXZhdGFycy80ODI2Mzc4MDUwMzQ2NjgwMzIvMzJmODFiMWFlNWRhYTM3YjRhYmM4ZDc0MWU0NWUzOTUud2VicCIsImF0dGFjaGVtZW50cyI6W10sImJvdCI6ZmFsc2V9LHsiY29udGVudCI6Ii0tZXhwb3J0IiwiYXV0aG9yIjoiTXJOaXojMjYyOCIsInRpbWVzdGFtcCI6MTY3NTY0NjI4MDI3MywiYXV0aG9ySWNvblVSTCI6Imh0dHBzOi8vY2RuLmRpc2NvcmRhcHAuY29tL2F2YXRhcnMvNDgyNjM3ODA1MDM0NjY4MDMyLzMyZjgxYjFhZTVkYWEzN2I0YWJjOGQ3NDFlNDVlMzk1LndlYnAiLCJhdHRhY2hlbWVudHMiOltdLCJib3QiOmZhbHNlfSx7ImNvbnRlbnQiOiItLWV4cG9ydCIsImF1dGhvciI6Ik1yTml6IzI2MjgiLCJ0aW1lc3RhbXAiOjE2NzU2NDYzMTIyNjMsImF1dGhvckljb25VUkwiOiJodHRwczovL2Nkbi5kaXNjb3JkYXBwLmNvbS9hdmF0YXJzLzQ4MjYzNzgwNTAzNDY2ODAzMi8zMmY4MWIxYWU1ZGFhMzdiNGFiYzhkNzQxZTQ1ZTM5NS53ZWJwIiwiYXR0YWNoZW1lbnRzIjpbXSwiYm90IjpmYWxzZX0seyJjb250ZW50IjoiLS1leHBvcnQiLCJhdXRob3IiOiJNck5peiMyNjI4IiwidGltZXN0YW1wIjoxNjc1NjQ2NTA3NTQ2LCJhdXRob3JJY29uVVJMIjoiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXZhdGFycy80ODI2Mzc4MDUwMzQ2NjgwMzIvMzJmODFiMWFlNWRhYTM3YjRhYmM4ZDc0MWU0NWUzOTUud2VicCIsImF0dGFjaGVtZW50cyI6W10sImJvdCI6ZmFsc2V9LHsiY29udGVudCI6Ii0tZXhwb3J0IiwiYXV0aG9yIjoiTXJOaXojMjYyOCIsInRpbWVzdGFtcCI6MTY3NTY0NjUxOTQ0OCwiYXV0aG9ySWNvblVSTCI6Imh0dHBzOi8vY2RuLmRpc2NvcmRhcHAuY29tL2F2YXRhcnMvNDgyNjM3ODA1MDM0NjY4MDMyLzMyZjgxYjFhZTVkYWEzN2I0YWJjOGQ3NDFlNDVlMzk1LndlYnAiLCJhdHRhY2hlbWVudHMiOltdLCJib3QiOmZhbHNlfSx7ImNvbnRlbnQiOiIiLCJhdXRob3IiOiJNck5peiMyNjI4IiwidGltZXN0YW1wIjoxNjc1NjQ2NjQwNjgxLCJhdXRob3JJY29uVVJMIjoiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXZhdGFycy80ODI2Mzc4MDUwMzQ2NjgwMzIvMzJmODFiMWFlNWRhYTM3YjRhYmM4ZDc0MWU0NWUzOTUud2VicCIsImF0dGFjaGVtZW50cyI6W3sidHlwZSI6ImltYWdlIiwidXJsIjoiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvMTA3MTg2NTcxMTIwMDMyNTcxMi8xMDcxOTY0NDQ4Nzc4MDk2NjcwL3BhMGxidWYxaXZsNTEuanBnIn1dLCJib3QiOmZhbHNlfSx7ImNvbnRlbnQiOiIiLCJhdXRob3IiOiJNck5peiMyNjI4IiwidGltZXN0YW1wIjoxNjc1NjQ2NjQ2Nzc1LCJhdXRob3JJY29uVVJMIjoiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXZhdGFycy80ODI2Mzc4MDUwMzQ2NjgwMzIvMzJmODFiMWFlNWRhYTM3YjRhYmM4ZDc0MWU0NWUzOTUud2VicCIsImF0dGFjaGVtZW50cyI6W3sidHlwZSI6ImltYWdlIiwidXJsIjoiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvMTA3MTg2NTcxMTIwMDMyNTcxMi8xMDcxOTY0NDczOTM1NTM2MTc4L2xvZ28tZGlzY29yZC5wbmcifV0sImJvdCI6ZmFsc2V9LHsiY29udGVudCI6Ii0tZXhwb3J0IiwiYXV0aG9yIjoiTXJOaXojMjYyOCIsInRpbWVzdGFtcCI6MTY3NTY0NjY0OTYzOSwiYXV0aG9ySWNvblVSTCI6Imh0dHBzOi8vY2RuLmRpc2NvcmRhcHAuY29tL2F2YXRhcnMvNDgyNjM3ODA1MDM0NjY4MDMyLzMyZjgxYjFhZTVkYWEzN2I0YWJjOGQ3NDFlNDVlMzk1LndlYnAiLCJhdHRhY2hlbWVudHMiOltdLCJib3QiOmZhbHNlfV0=" }
+
+$.ajax("https://cdn.discordapp.com/attachments/1017554575718359101/1071887849357594735/guild.txt",   // request url
+    {
+        success: function (data, status, xhr) {// success callback function
+            // $('p').append(data);
+            console.log(data)
+            readData(data)
+    }
+});
+const params = Object.fromEntries(new URLSearchParams(window.location.search).entries());
+let defaultDate = 1675642910818;
+let defaultUserName = "Deleted User#0000";
+let defaultBot = true;
+
+function readData(dataStr)
+{
+    const req = JSON.parse(dataStr)
+    
+    const header = document.getElementById("header")
+    header.getElementsByTagName("img")[0].src = req.guildIcon
+    header.getElementsByTagName("h1")[0].innerText = req["guildName"] + " (" + req["guildId"] + ")"
+    header.getElementsByTagName("h2")[0].innerText = req["channel"]
+}
+/**
+ * exportSave
+ */
+// ''
+// { "guildName": "Mizyico Community Discord", "guildId": "-1", "guildIcon": "https://images-ext-1.discordapp.net/external/-AV0NhM60fnxWHkccLXrqESsncXjzWCxXeKszWdvbHY/%3Fsize%3D2048/https/cdn.discordapp.com/icons/973021942892359711/daed5d7cdac059451354c820f3a557ec.webp", "channel": "🚗|carros-mamalones"}
+// atob(params.guild)
+readData( atob(data.guild))
+
+const main = document.getElementsByTagName("main")[0];
+function request(link){
+    $.ajax(link,   // request url
+    {
+        success: function (data, status, xhr) {// success callback function
+            // $('p').append(data);
+            // console.log(data)
+            console.log(data);
+            return data
+    }
+});
+console.log("hola");
+
+return "Can't get data from \""+ link+ "\" :("
+}
+let curMsg = 0;
+console.log(new Date().getTime())
+let json;
+
+try {
+    json = JSON.parse(atob(data.messages));
+    console.log("a")
+} catch(e) {
+    try {
+    console.log("a :/")
+
+         json = JSON.parse(data.messages);
+    } catch (er)
+    {
+        console.log(e)
+    }
+
+}
+console.log(JSON.stringify(json))
+// console.log(atob('[{"content":"hola","author":"MrNiz","authorIconURL":"a","attachements":[],"timestamp":null,"bot":false},{}]'))
+for (let data of json){
+setMessage({content: data.content, author: data.author || defaultUserName, authorIconURL: data.authorIconURL, attachements: data.attachements || [], timestamp: data.timestamp || defaultDate, bot: data.bot})
+}
+
+// {{content: String, author:String, timestamp:Number, authorIconURL:String, attachements:Array<{type:String, url:String}}
+/**
+ * 
+ * @param {{content: String, author:String, timestamp:Number, authorIconURL:String, attachements:Array<{type:String, url:String}>}} data 
+ */
+function setMessage(data)
+{
+    if (!data.bot && data.bot !== false)
+        data.bot = defaultBot;
+    let date = new Date(data.timestamp);
+    let div = document.createElement("div")
+    let imgs = "<br></br>";
+    for (const attachement of data.attachements)
+    {
+        console.log(attachement.type )
+       if ( attachement.type !== "image")
+       {
+        
+        imgs += `\n
+        <atch class="attachement_txt" >
+        <div class="attachement_text">
+          <p>
+          ${request(attachement.url)}
+          </p>
+        </div>
+      </atch>
+        `
+       }
+       else
+       {
+        imgs +=  `
+        \n
+        <img class="attachement_img" src="${attachement.url}"></img>
+        `;
+       }
+    }
+    console.log(data.bot)
+    div.innerHTML = ` 
+    <div id="${curMsg}-msg" class="simple_message">
+    <img class="pfp" src='${data.authorIconURL}' onerror="this.src = 'img/discord.png'"></img>
+    <author>${data.author}</author>
+    ${data.bot ? `<img class="bot" src="https://cdn3.emoji.gg/emojis/1646-discord-bot-en.png"></img>` : ""}
+    <timestamp>${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} - ${date.getDay()}/${date.getMonth()}/${date.getFullYear()}</timestamp>
+    <br></br>
+    <msg>${data.content}</msg>
+    <br></br>
+    ${imgs !== "<br></br>" ? imgs : ""}
+    <line> </line>
+    
+    </div>`
+    main.appendChild(div);
+}
